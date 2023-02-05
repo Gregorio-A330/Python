65431c4c0fccf9c74c299d636f1e2cae03432d65
Gabriel Couto <gabriel.gregorio@devmagic.com.br>
2023-02-05 19:20:10 -0300

    Starting again with Python

---
+++ StartingWithPython/Testes/teste1.py
@@ -0,0 +1,31 @@
+print("Testando código da primeira aula")
+
+# pais = "Brasil"
+# quantidade = 5      ##tipagem dinamica 
+
+# print(pais, "ganhou", quantidade, "titulos mundiais", end="!")
+
+
+
+class TestDat():          # leave this empty
+    def __init__(self):   # constructor function using self
+        self.Dat1 = None  # variable using self.
+        self.Dat2 = None  # variable using self
+    
+TestArray = [] #empty array
+
+Test1 = TestDat() #this is an object
+Test2 = TestDat() #this is another object
+        
+Test1.Dat1 = 0 #assigning value to object 1 
+Test1.Dat2 = 1 #assigning value to object 1 
+    
+Test2.Dat1 = 3 #assigning value to object 2 
+Test2.Dat2 = 4 #assigning value to object 2
+
+TestArray.append(Test1) #append object 1
+TestArray.append(Test2) #append object 2 
+    
+print ("aaaaa", TestArray[0]) # this is Test1
+# print (TestArray[0].Dat1) # this is Test1
+# print (TestArray[1].Dat1) # this is Test2
---
+++ StartingWithPython/jogos/adivinhacao.py
@@ -0,0 +1,42 @@
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+
+numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
+
+while(rodada <= total_de_tentativas):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas)) # string interpolation
+    
+    chute_str = input("Digite o seu numero: ")
+    chute = int(chute_str)
+
+    print("você digitou ", chute_str, type(chute_str)) ## input retorna a entrada como string e é necessário converter
+
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if (acertou):  # o dois pontos é inicio de um bloco de execução
+    # if (int(chute_str) == numero_secreto):  # OU
+        print("voce acertou!")
+        break
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior do que o número secreto.")
+        elif(menor):
+            print("Você errou! O seu chute foi menor do que o número secreto.")
+    rodada = rodada + 1
+
+print("Fim de jogo...")
+
+
+# Diferenças entre python 2 e python 3 para >>>>> print e input()
+#  não tinha obrigatoriedade de parenteses no python 2 e o print do 3 aceita mais parametros como o sep="" e o end=""
+#  não converte diretamente o input no python 3 porque no python 2 era considerado má pratica
+
+# raw_input só exite no python 2 e trazia a entrada diretamente sem conversão não existe no python 3
+
+
+# elif -> else if()
